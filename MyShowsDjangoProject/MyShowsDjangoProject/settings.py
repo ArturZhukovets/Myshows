4d69226fdad8c5c26c337fc924fdb2ef95386918
Zhukovec Artur <bnlvdn7483@gmail.com>
2022-08-10 02:38:12 +0300

    [upd 3] creating views, working with templates

--- MyShowsDjangoProject/MyShowsDjangoProject/settings.py
+++ MyShowsDjangoProject/MyShowsDjangoProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'MyShowsDjangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -114,10 +114,16 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = BASE_DIR / 'static'
+STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- MyShowsDjangoProject/movies/views.py
+++ MyShowsDjangoProject/movies/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from django.views.generic.base import View
+from .models import Movie
+
+
+
+class MoviesView(ListView):
+    """Список фильмов"""
+    model = Movie
+    template_name = "movies/movie_list.html"    # Ищет по дефолту шаблон movie_list.html
+    queryset = Movie.objects.filter(draft=False)
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    template_name = "movies/movie_detail.html"
+    slug_field = "url"
+    # def get(self, request, slug):
+    #     movie = Movie.objects.get(url=slug)
+    #     return render(request, "movies/movie_detail.html", context={"movie":movie})
